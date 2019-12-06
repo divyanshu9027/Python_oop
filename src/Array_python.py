28d044eda60ee3c0e8e2bd5d32ef3e67378d8b13
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2019-12-06 08:29:33 -0800

    done

---
+++ src/Array_python.py
@@ -0,0 +1,9 @@
+class Array_python:
+    def __init(self,name,price):
+        self.name=name
+        self.price=price
+name=["maruti","honda","hyundai","toyota","tata"]
+price=["4","5","7","8","9"]
+print(name[2])
+print(name)
+
